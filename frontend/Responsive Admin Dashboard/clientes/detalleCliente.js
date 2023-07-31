44b842b39eddb3a6e2ee58c438fed46a4ea73fa2
pepitoperez <pepitoperez>
2023-07-31 17:06:45 -0500

    feat: :lipstick: FORMULARIO LISTO PARA AGREGAR UPDATE

--- frontend/Responsive Admin Dashboard/clientes/detalleCliente.js
+++ frontend/Responsive Admin Dashboard/clientes/detalleCliente.js
@@ -2,10 +2,15 @@ import {
     getOneClientes
 } from "../Responsive Admin Dashboard - final/assets/js/API.js";
 
+/* Selectores */
 const nombreClienteTitle = document.getElementById('nombreClienteTitle');
 document.addEventListener('DOMContentLoaded', loadData());
 const titleForm = document.getElementById('titleForm');
-
+const nombreClienteFormulario = document.getElementById('nombreClienteFormulario');
+const identificacionClienteFormulario = document.getElementById('identificacionClienteFormulario');
+const telefonoClienteFormulario = document.getElementById('telefonoClienteFormulario');
+const emailClienteFormulario = document.getElementById('emailClienteFormulario');
+const direccionClienteFormulario = document.getElementById('direccionClienteFormulario');
 
 async function loadData() {
     try {
@@ -17,6 +22,11 @@ async function loadData() {
 
         nombreClienteTitle.textContent = `${cliente.nombre}`;
         titleForm.textContent = `${cliente.nombre}`;
+        nombreClienteFormulario.value = `${cliente.nombre}`;
+        identificacionClienteFormulario.value = `${cliente.identificacion}`;
+        telefonoClienteFormulario.value = `${cliente.telefono}`;
+        emailClienteFormulario.value = `${cliente.mail}`;
+        direccionClienteFormulario.value = `${cliente.direccion}`;
     } catch (error) {
         console.log(error);
     } finally {
